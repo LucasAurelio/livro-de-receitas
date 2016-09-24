ff04dc0ba04a40e5a62fb634291613e1c7884cbd
Michelly Menezes <martha.michelly@gmail.com>
2016-09-24 16:41:48 -0300

    Help Fragment

---
+++ app/src/main/java/com/projetoes/livrodereceitas/fragments/HelpFragment.java
@@ -0,0 +1,52 @@
+package com.projetoes.livrodereceitas.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.projetoes.livrodereceitas.R;
+
+public class HelpFragment extends Fragment {
+
+    private static HelpFragment fragment;
+    public static final String TAG = "HELP_FRAGMENT";
+
+    public HelpFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment SearchFragment.
+     */
+    public static HelpFragment getInstance() {
+        if (fragment == null) {
+            fragment = new HelpFragment();
+            Bundle args = new Bundle();
+            fragment.setArguments(args);
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_help, container, false);
+
+
+        return view;
+    }
+    
+}
